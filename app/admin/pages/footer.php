8fde40156b36a3e1e6d7fd5aaceed2f7cfa3c0de
dwisaputra32 <dwis30614@gmail.com>
2024-02-26 11:31:54 +0700

    first commit

---
+++ app/admin/pages/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> 1.1
+    </div>
+    <strong>Hak Cipta &copy; <?= date('Y'); ?>. RPL Team</strong> PT SMK SATRYA BUDI KARANG REJO | Repost by Dwi<a href='https://dwisaputra302.blogspot.com/' title='dwisaputra302.blogspot.com' target='_blank'>WebSite.</a>
+</footer>
---
+++ app/user/pages/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> 1.1
+    </div>
+    <strong>Hak Cipta &copy; <?= date('Y'); ?></strong>  Repost by Dwi Saputra<a href='https://dwisaputra302.blogspot.com/' title='dwisaputra302.blogspot.com' target='_blank'>WebSite.</a>
+    
+</footer>
